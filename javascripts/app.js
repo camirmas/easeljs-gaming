ee6f9cebf70f4d211d831ce8ec172692ab96c4e0
Cameron Irmas <cjirmas@gmail.com>
2015-01-20 11:07:51 -0800

    change to cam sprite, add coin game

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,31 +1,42 @@
 var character, characterIdle, currentCharacter;
 var stage;
+var score = 0;
+var coins = [];
+var text = new createjs.Text("Score: " + score, "20px Arial", "#ff7700");
 
 function init() {
   stage = new createjs.Stage("demoCanvas");
 
-  var dataLeft = {
+  var dataCoin = {
     "framerate": 30,
-    "images": ["img/monster.png"],
-    "frames": { width: 64, height: 64 },
+    "images": ["img/coin.png"],
+    "frames": { width: 50, height: 50 },
     "animations": {
-      "run": [0, 9]
+      "spin": [0,9]
+    }
+  }
+
+  var dataLeft = {
+    "framerate": 10,
+    "images": ["img/cam_pixel_left.png"],
+    "frames": { width: 123.67, height: 150 },
+    "animations": {
+      "run": [0, 1]
     }
   };
 
   var dataRight = {
-    "framerate": 30,
-    "images": ["img/monster_right.png"],
-    "frames": { width: 64, height: 64 },
+    "framerate": 10,
+    "images": ["img/cam_pixel_right.png"],
+    "frames": { width: 127, height: 150 },
     "animations": {
-      "run": [0, 9]
+      "run": [0, 1]
     }
   };
 
   var dataIdle = {
-    "framerate": 30,
-    "images": ["img/monster_idle.png"],
-    "frames": { width: 64, height: 64 },
+    "images": ["img/cam_pixel.png"],
+    "frames": { width: 121, height: 150 },
     "animations": {
       "stand": 0
     }
@@ -38,38 +49,88 @@ function init() {
   characterI = new createjs.Sprite(spriteSheetI, "stand");
   characterL = new createjs.Sprite(spriteSheetL, "run");
   characterR = new createjs.Sprite(spriteSheetR, "run");
-
-  characterI.x, characterL.x, characterR.x = stage.canvas.width / 2;
-  characterI.y, characterL.y, characterR.y = 50;
-
   currentCharacter = characterI;
 
+  characterI.x = stage.canvas.width / 2;
+  characterI.y = 50;
+
   stage.addChild(currentCharacter);
+  stage.addChild(text);
+
+
+  for (var i = 0; i < 10; i++) {
+    var spriteSheetCoin = new createjs.SpriteSheet(dataCoin);
+    coin = new createjs.Sprite(spriteSheetCoin, "spin");
+    coin.x = Math.random() * 800;
+    coin.y = Math.random() * 400;
+    coins.push(coin);
+    stage.addChild(coin);
+  }
+
+  var event = new Event("coin");
+  createjs.Ticker.addEventListener("coin", nearCoin);
+
   createjs.Ticker.timingMode = createjs.Ticker.RAF;
   createjs.Ticker.addEventListener("tick", stage);
   createjs.Ticker.addEventListener("tick", move);
+  createjs.Ticker.addEventListener("tick", scoreAndRemoveCoin);
+}
+
+function nearCoin() {
+  for(var i = 0; i < coins.length; i++) {
+    var coin = coins[i];
+
+    if (coin) {
+      var distance = Math.sqrt(Math.pow((currentCharacter.x - coin.x), 2) + Math.pow((currentCharacter.y - coin.y), 2));
+
+      if (distance < 80) {
+        return coin;
+      }
+    }
+  }
+  return false;
+}
+
+function scoreAndRemoveCoin() {
+  if (nearCoin()) {
+    var coin = nearCoin();
+    score += 100;
+    stage.removeChild(coin);
+    delete coins[coins.indexOf(coin)];
+    changeScoreText();
+  }
+}
+
+function changeScoreText() {
+  stage.removeChild(text);
+  text = new createjs.Text("Score: " + score, "20px Arial", "#ff7700");
+  stage.addChild(text);
 }
 
 function move(event) {
   if (key.isPressed('left')) {
-    characterL.x = currentCharacter.x;
-    characterL.y = currentCharacter.y;
-    stage.removeChild(currentCharacter);
-    currentCharacter = characterL;
-    stage.addChild(currentCharacter);
+    changeAnimation(characterL);
     currentCharacter.x -= event.delta/1000 * 100;
+
   } else if (key.isPressed('right')) {
-    characterR.x = currentCharacter.x;
-    characterR.y = currentCharacter.y;
-    stage.removeChild(currentCharacter);
-    currentCharacter = characterR;
-    stage.addChild(currentCharacter);
+    changeAnimation(characterR);
     currentCharacter.x += event.delta/1000 * 100;
+
+  } else if (key.isPressed('up')) {
+    currentCharacter.y -= event.delta/1000 * 100;
+
+  } else if (key.isPressed('down')) {
+    currentCharacter.y += event.delta/1000 * 100;
+
   } else {
-    characterI.x = currentCharacter.x;
-    characterI.y = currentCharacter.y;
-    stage.removeChild(currentCharacter);
-    currentCharacter = characterI;
-    stage.addChild(currentCharacter);
+    changeAnimation(characterI);
   }
+}
+
+function changeAnimation(character) {
+  character.x = currentCharacter.x;
+  character.y = currentCharacter.y;
+  stage.removeChild(currentCharacter);
+  currentCharacter = character;
+  stage.addChild(currentCharacter);
 }
